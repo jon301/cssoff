bb5a6a41de297ca0f1caa0943c03b5cc3a8eb012
Jonathan Trang <jon301@gmail.com>
2011-11-01 17:16:48 +0100

    Nav transitions. Obstacles.

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Obstacles
+    var obs_items = $('#obstacles li');
+    var obs_slides = $('#obs_slides .slide');
+    obs_items.hover(function() {
+        var $this = $(this);
+        obs_items.removeClass('selected');
+        $this.addClass('selected');
+
+        obs_slides.removeClass('selected');
+        obs_slides.filter('[data-obs-id="' + $this.data('obs-id') + '"]').addClass('selected');
+    });
+});
